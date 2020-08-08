91b1a67d00b64d1aa47739e8ae04875d66a5e60b
Juanjo <juanjo@juanjofp.com>
2020-08-08 13:08:03 +0200

    add samples for rules

--- src/pages/rule-list/create-dialog/index.tsx
+++ src/pages/rule-list/create-dialog/index.tsx
@@ -20,29 +20,29 @@ type RuleTypesSamples = {[key in RuleTypes]: string[][]};
 const subtitles: RuleTypesText = {
     realtime: ['This rule executes one time for each event', 'On event payload of a given type, each rule will be evaluated to determinate if the event payload match the rule filter or does not.'],
     sliding: ['This rule executes one time for each event aggregating previous events', 'On event payload of a given type, sliding rule evaluates a filter match based on an aggregation of a given time window. Aggregation operators supported are max, min, count, avg, stdDevPop, stdDevSample.'],
-    hopping: ['This rule executes...', 'compute windowing with...'],
-    tumbling: ['This rule executes in fixed time intervals aggregating events reveived inside the interval', 'As sliding rules, tumbling rules perform an aggregation of a given time window. However, in spite of realtime and slinding rules, tumbling rules got executed on a given time interval. This time interval matches the time window']
+    tumbling: ['This rule executes in fixed time intervals aggregating events reveived inside each interval', 'As sliding rules, tumbling rules perform an aggregation of a given time window. However, in spite of realtime and slinding rules, tumbling rules got executed on a given time interval. This time interval matches the time window'],
+    hopping: ['This rule executes each custom time interval aggregatting events occurred in a fixed time interval', 'compute windowing with...']
 };
 const samples: RuleTypesSamples = {
     realtime: [
-        ['temperature > 10', 'checks temperature is great than 10 for each temperature received'],
-        ['distance(location) > 50', 'checks a locations is great than 5 meters'],
-        ['battery < 30 and windSpeed > 5', 'checks battery is less than 30 and windSpeed great than 5']
+        ['temperature > 10', 'Checks temperature is great than 10 for each temperature received'],
+        ['distance(location) > 50', 'Checks a locations is great than 5 meters'],
+        ['battery < 30 and windSpeed > 5', 'Checks battery is less than 30 and windSpeed great than 5']
     ],
     sliding: [
-        ['avg(temperature) > 35 last hour', 'checks temperature average is greater than 35 degrees on the last hour'],
-        ['sum(count) > 100 last 30 seconds', 'checks count sum is greater than 100 on the last 30 seconds'],
-        ['max(prints) < 10 last 90 minutes', 'checks max prints is less than 10 on the last 90 minutes']
-    ],
-    hopping: [
-        ['Example 1', 'explanation example 1'],
-        ['Example 2', 'explanation example 2'],
-        ['Example 3', 'explanation example 3']
+        ['avg(temperature) > 35 last hour', 'Checks temperature average is greater than 35 degrees on the last hour'],
+        ['sum(count) > 100 last 30 seconds', 'Checks count sum is greater than 100 on the last 30 seconds'],
+        ['max(prints) < 10 last 90 minutes', 'Checks max prints is less than 10 on the last 90 minutes']
     ],
     tumbling: [
         ['count(temperature) = 0 last 5 minutes', 'Checks no events of temperature has arrived every 5 minutes'],
         ['avg(temperature) > 30 last 5 minutes', 'Checks average of temperature of temperature is great than 30 every 5 minutes'],
         ['max(temperature) > 10 last 5 minutes', 'Checks max value of temperature is great than 30 every 5 minutes'],
+    ],
+    hopping: [
+        ['count(temperature) = 0 each 5 minutes in 10 minutes', 'Checks no events of temperature has arrived every 5 minutes'],
+        ['avg(temperature) > 30 each 15 minutes in 1 hour', 'Checks average of temperature of temperature is great than 30 every 5 minutes'],
+        ['max(temperature) > 10 each 5 seconds in 1 minute', 'Checks max value of temperature is great than 30 every 5 minutes'],
     ]
 };
 
@@ -93,8 +93,8 @@ export const CreateRuleDialog: React.FC<CreateRuleDialogProps> = ({isOpen, onClo
                 aria-label='kind of rules description'>
                 <RuleTypeCard type='realtime' ariaLabel='create rule real time card' selected={type === 'realtime'} onClick={() => selectType('realtime')}/>
                 <RuleTypeCard type='sliding' ariaLabel='create rule sliding card' selected={type === 'sliding'} onClick={() => selectType('sliding')}/>
-                <RuleTypeCard type='tumbling' ariaLabel='create rule tumbling card' selected={type === 'tumbling'} onClick={() => selectType('tumbling')}/>
                 <RuleTypeCard type='hopping' ariaLabel='create rule hopping card' selected={type === 'hopping'} onClick={() => selectType('hopping')}/>
+                <RuleTypeCard type='tumbling' ariaLabel='create rule tumbling card' selected={type === 'tumbling'} onClick={() => selectType('tumbling')}/>
             </DialogContent>
             <DialogContent>
 
